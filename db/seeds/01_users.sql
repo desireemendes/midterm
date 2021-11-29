9921ecaf8d00c81de388efd6b0e0ccf2425df4a4
RAJINIA <rajini1608@gmail.com>
2021-11-28 22:43:58 -0600

    Add menus table and seed to db

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,35 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+
+
+-- Menus table
+INSERT INTO menus (id, picture_url, name, description, price, active)
+VALUES (1, 'https://images.app.goo.gl/ah3seX84CfsxhZ5e6', 'Samosa', 'Triangle shaped pastries filled with onions, peas and a variety of Indian spices with your choice of potato, lentils, chicken or beef filling', 4.99, true);
+
+INSERT INTO menus (id, picture_url, name, description, price, active)
+VALUES (3, 'https://images.app.goo.gl/wVrXwLfoPqCzUDcc6', 'Vegetable Pakoras', 'Cauliflower, cabbage, potato, spinach and onion mixed with a chickpea batter and deep-fried in vegetable oil', 3.99, true);
+
+INSERT INTO menus (id, picture_url, name, description, price, active)
+VALUES (5, 'https://images.app.goo.gl/oX54LrLKanBnE9cE9', 'Tandoori Chicken Wings', 'Chicken wings marinated in yogurt, garlic, ginger and spices & cooked in our clay oven', 6.99, true);
+
+INSERT INTO menus (id, picture_url, name, description, price, active)
+VALUES (7, 'https://images.app.goo.gl/i4qJgsuMc8fVBVvDA', 'Fish Pakora', 'Breaded fillets of fresh basa seasoned with Indian spices', 6.99, true);
+
+INSERT INTO menus (id, picture_url, name, description, price, active)
+VALUES (9, 'https://images.app.goo.gl/rSs1prVruyBtp3Zx5', 'Biriyani(Vegetables/Paneer/Chicken, Lamb, Prawn or Beef)', 'Saffron rice cooked with vegetables/paneer/chicken/lamb/beef/prawns with whole spices, almonds, cashews, and dairy', 20, true);
+
+
+INSERT INTO menus (id, picture_url, name, description, price, active)
+VALUES (2, 'https://images.app.goo.gl/FeaaqzJYMLq4ZUdA6', 'Kadai Paneer', 'Homemade cheese cooked with whole spices, onions, green and red bell peppers', 14.99, true);
+
+INSERT INTO menus (id, picture_url, name, description, price, active)
+VALUES (4, 'https://images.app.goo.gl/XwaudvhZJny5fTvJ6', 'Butter Chicken', 'Deboned tandoori chicken cooked in a tomato and cream based sauce', 17.49, true);
+
+INSERT INTO menus (id, picture_url, name, description, price, active)
+VALUES (6, 'https://images.app.goo.gl/38MnKozcgePcNuBC7', 'Ras Malai', 'Soft Indian homemade cheese cooked in syrup topped with a creamy sauce and a sprinkle of nuts', 8.59, true);
+
+INSERT INTO menus (id, picture_url, name, description, price, active)
+VALUES (8, 'https://images.app.goo.gl/DC3rp8HAvKiGHPKV6', 'Kulfi (Mango/Pistachio/Almond)', 'Homemade Indian ice cream on a stick, garnished with nut', 4.5, true);
+
+INSERT INTO menus (id, picture_url, name, description, price, active)
+VALUES (10, 'https://images.app.goo.gl/H3aTHtE9zcM4ZQ4r9 ', 'Masala Chai', 'Black tea infused with fragrant spices, typically served with milk', 3.5, true);
+
+
