abe65e2b4b8e39a636f5c4edeb7e56a0c2f1462a
RAJINIA <rajini1608@gmail.com>
2021-11-30 17:55:37 -0600

    Update HomePage, LoginPage and MenuPage

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,7 +1,3 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name) VALUES ('Alice');
-<<<<<<< HEAD
-INSERT INTO users (name) VALUES ('Kira')
-=======
 INSERT INTO users (name) VALUES ('Kira');
->>>>>>> 009950cacafd18d6957167463341ffd881d421a2
--- db/seeds/menuSeeds.sql
+++ db/seeds/menuSeeds.sql
@@ -1,31 +1,30 @@
-
+INSERT INTO menus (id, picture_url, name, description, price, active)
+VALUES (1, 'https://image.shutterstock.com/image-photo/vegetarian-samsa-samosasindian-special-traditional-600w-1194515290.jpg', 'Samosa', 'Triangle shaped pastries filled with onions, peas and a variety of Indian spices with your choice of potato, lentils, chicken or beef filling', 4.99, true);
 
 INSERT INTO menus (id, picture_url, name, description, price, active)
-VALUES (1, 'https://images.app.goo.gl/ah3seX84CfsxhZ5e6', 'Samosa', 'Triangle shaped pastries filled with onions, peas and a variety of Indian spices with your choice of potato, lentils, chicken or beef filling', 4.99, true);
+VALUES (3, 'https://www.aspicyperspective.com/wp-content/uploads/2020/05/Vegetable-Pakora-Recipe-with-Kale-12.jpg', 'Vegetable Pakoras', 'Cauliflower, cabbage, potato, spinach and onion mixed with a chickpea batter and deep-fried in vegetable oil', 3.99, true);
 
 INSERT INTO menus (id, picture_url, name, description, price, active)
-VALUES (3, 'https://images.app.goo.gl/wVrXwLfoPqCzUDcc6', 'Vegetable Pakoras', 'Cauliflower, cabbage, potato, spinach and onion mixed with a chickpea batter and deep-fried in vegetable oil', 3.99, true);
+VALUES (5, 'https://spicecravings.com/wp-content/uploads/2020/12/Tandoori-Chicken-Wings-1.jpg', 'Tandoori Chicken Wings', 'Chicken wings marinated in yogurt, garlic, ginger and spices & cooked in our clay oven', 6.99, true);
 
 INSERT INTO menus (id, picture_url, name, description, price, active)
-VALUES (5, 'https://images.app.goo.gl/oX54LrLKanBnE9cE9', 'Tandoori Chicken Wings', 'Chicken wings marinated in yogurt, garlic, ginger and spices & cooked in our clay oven', 6.99, true);
+VALUES (7, 'https://nishkitchen.com/wp-content/uploads/2021/10/Fish-Pakora-2B.jpg', 'Fish Pakora', 'Breaded fillets of fresh basa seasoned with Indian spices', 6.99, true);
 
 INSERT INTO menus (id, picture_url, name, description, price, active)
-VALUES (7, 'https://images.app.goo.gl/i4qJgsuMc8fVBVvDA', 'Fish Pakora', 'Breaded fillets of fresh basa seasoned with Indian spices', 6.99, true);
+VALUES (9, 'https://www.dokan.co.in/core/uploads/2021/02/Mutton-Biryani.jpg', 'Biriyani(Vegetables/Paneer/Chicken, Lamb, Prawn or Beef)', 'Saffron rice cooked with vegetables/paneer/chicken/lamb/beef/prawns with whole spices, almonds, cashews, and dairy', 20, true);
 
 INSERT INTO menus (id, picture_url, name, description, price, active)
-VALUES (9, 'https://images.app.goo.gl/rSs1prVruyBtp3Zx5', 'Biriyani(Vegetables/Paneer/Chicken, Lamb, Prawn or Beef)', 'Saffron rice cooked with vegetables/paneer/chicken/lamb/beef/prawns with whole spices, almonds, cashews, and dairy', 20, true);
+VALUES (2, 'https://www.cubesnjuliennes.com/wp-content/uploads/2020/03/Best-Kadai-Paneer-Recipe.jpg', 'Kadai Paneer', 'Homemade cheese cooked with whole spices, onions, green and red bell peppers', 14.99, true);
 
 INSERT INTO menus (id, picture_url, name, description, price, active)
-VALUES (2, 'https://images.app.goo.gl/FeaaqzJYMLq4ZUdA6', 'Kadai Paneer', 'Homemade cheese cooked with whole spices, onions, green and red bell peppers', 14.99, true);
+VALUES (4, 'https://images.immediate.co.uk/production/volatile/sites/30/2021/02/butter-chicken-ac2ff98.jpg', 'Butter Chicken', 'Deboned tandoori chicken cooked in a tomato and cream based sauce', 17.49, true);
 
 INSERT INTO menus (id, picture_url, name, description, price, active)
-VALUES (4, 'https://images.app.goo.gl/XwaudvhZJny5fTvJ6', 'Butter Chicken', 'Deboned tandoori chicken cooked in a tomato and cream based sauce', 17.49, true);
+VALUES (6, 'https://www.indianhealthyrecipes.com/wp-content/uploads/2014/10/diwali-sweets-recipe-rasmalai.jpg', 'Ras Malai', 'Soft Indian homemade cheese cooked in syrup topped with a creamy sauce and a sprinkle of nuts', 8.59, true);
 
 INSERT INTO menus (id, picture_url, name, description, price, active)
-VALUES (6, 'https://images.app.goo.gl/38MnKozcgePcNuBC7', 'Ras Malai', 'Soft Indian homemade cheese cooked in syrup topped with a creamy sauce and a sprinkle of nuts', 8.59, true);
+VALUES (8, 'https://www.ruchiskitchen.com/wp-content/uploads/2015/05/Mango-Kulfi-Recipe-4-1.jpg', 'Kulfi (Mango/Pistachio/Almond)', 'Homemade Indian ice cream on a stick, garnished with nut', 4.5, true);
 
 INSERT INTO menus (id, picture_url, name, description, price, active)
-VALUES (8, 'https://images.app.goo.gl/DC3rp8HAvKiGHPKV6', 'Kulfi (Mango/Pistachio/Almond)', 'Homemade Indian ice cream on a stick, garnished with nut', 4.5, true);
+VALUES (10, 'https://www.theflavorbender.com/wp-content/uploads/2021/01/Masala-Chai-9998-2.jpg', 'Masala Chai', 'Black tea infused with fragrant spices, typically served with milk', 3.5, true);
 
-INSERT INTO menus (id, picture_url, name, description, price, active)
-VALUES (10, 'https://images.app.goo.gl/H3aTHtE9zcM4ZQ4r9 ', 'Masala Chai', 'Black tea infused with fragrant spices, typically served with milk', 3.5, true);
