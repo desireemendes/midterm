776dbceb14e3ac79fc8d38a11aecdac81091bfb3
Desiree Mendes <desireefrost@live.ca>
2021-11-30 21:41:07 -0500

    Twilio functions setup for sms

---
+++ routes/sms-send.js
@@ -0,0 +1,19 @@
+// Twilio Credentials
+// To set up environmental variables, see http://twil.io/secure
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+
+
+// require the Twilio module and create a REST client
+const client = require('twilio')(accountSid, authToken);
+
+
+client.messages
+  .create({
+    to: +1,
+    from: '+12078172429',
+    body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+  })
+  .then(message => console.log(message.sid));
+
